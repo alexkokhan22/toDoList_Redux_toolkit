2bba02edfe7b37e71bfda9fab8b3954a76e5d769
Alex Kokhan <kokhan.a.a@yandex.by>
2021-06-21 17:01:19 +0300

    add api-key

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ import {GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType,
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '96882b02-a427-4592-b196-6833093c647f'
     }
 }
 const instance = axios.create({
